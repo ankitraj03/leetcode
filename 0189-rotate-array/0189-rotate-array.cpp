efb65342ee3772b0a3046dcc01b122162618a997
Ankit Raj Sinha <97229534+ankitraj03@users.noreply.github.com>
2025-03-21 00:30:18 +0530

    Time: 0 ms (100%), Space: 29.4 MB (79.89%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        if(nums.size()<=1)
-        return;
-        if(k>nums.size())
         k=k%nums.size();
         reverse(nums.begin(), nums.end());
         reverse(nums.begin(),nums.begin()+k);
