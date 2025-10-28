73c223a83acf71421e97362bb45aa533e7faef49
Ankit Raj Sinha <97229534+ankitraj03@users.noreply.github.com>
2025-10-28 19:28:51 +0530

    Time: 0 ms (100%), Space: 20.3 MB (84.86%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,26 +1,7 @@
 class Solution {
 public:
-    // int jump(vector<int>& nums) {
 
-        // my code- got TLE
-    //     int count=0, ans=INT_MAX;
-    //     find(nums,0, count, ans);
-    //     return ans;
-    // }
-    // void find(vector<int> nums, int i, int count, int & ans){
-    //     int n=nums.size();
-    //     if(i>=n)
-    //    return;
-
-    //    if(i==n-1){
-    //     ans=min(count, ans);
-    //     return;
-    //    }
-    //    for(int j=1;j<=nums[i];j++){
-    //     find(nums, j+i, count+1, ans);
-    //    } 
-
-    int jump(vector<int>& nums) {
+           int jump(vector<int>& nums) {
         int jumps = 0, current_end = 0, farthest = 0;
         
         for (int i = 0; i < nums.size() - 1; ++i) {
